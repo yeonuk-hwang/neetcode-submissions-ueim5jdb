427192517e4eb39e2b127f1b1acd7108172b490d
Yeonuk Hwang <charlie.hwang131@gmail.com>
2026-04-13 23:30:05 +0930

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # get the length of s1
+        # iterate s2 with fixed size sliding window
+        # if the character count in sliding window is same as s1, then return true
+        # if it reaches the end, return false
+
+        s1_length = len(s1)
+        count = Counter(s1) # O(s1)
+
+        left = 0
+
+        for right in range(left + s1_length, len(s2) + 1):
+            print("left", left, "right", right)
+            window_count = Counter(s2[left:right])
+            print("window_count", window_count)
+            if window_count == count:
+                return True
+            left += 1
+
+        return False
