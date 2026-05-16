6d82f3a698b485468dfa8cdc67c7cc877381ceaf
Yeonuk Hwang <charlie.hwang131@gmail.com>
2026-05-16 14:01:01 +0930

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.go
@@ -0,0 +1,30 @@
+func isPalindrome(s string) bool {
+	rs := []rune(s)
+	start, end := 0, len(rs)-1
+
+	for start < end {
+		front := rs[start]
+		if !isAlphaNumeric(front) {
+			start++
+			continue
+		}
+		rear := rs[end]
+		if !isAlphaNumeric(rear) {
+			end--
+			continue
+		}
+
+		if unicode.ToLower(front) != unicode.ToLower(rear) {
+			return false
+		}
+
+		start++
+		end--
+	}
+
+	return true
+}
+
+func isAlphaNumeric(ch rune) bool {
+	return unicode.IsDigit(ch) || unicode.IsLetter(ch)
+}
